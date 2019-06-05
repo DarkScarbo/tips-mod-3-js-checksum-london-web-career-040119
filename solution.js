c3b68388d5db9da1193500db2872b97192d63ae7
github username <github email address>
2019-06-05 16:46:32 +0100

    Done.

--- solution.js
+++ solution.js
@@ -1,1 +1,14 @@
+const data = [
+  [409, 194, 207, 470, 178],
+  [454, 235, 333, 511, 103],
+  [474, 293, 525, 372, 408],
+  [428, 4321, 2786, 6683, 3921],
+  [265, 262, 6206, 2207, 5712]
+];
 
+function checksum(data) {
+  rowsArray = data.map(
+    rowArray => Math.max(...rowArray) - Math.min(...rowArray)
+  );
+  return rowsArray.reduce((a, b) => a + b);
+}
